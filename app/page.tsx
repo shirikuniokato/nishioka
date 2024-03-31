7b2fa2dc4da1ebe7e6e2a7165661360218df4c1d
rikuto-nishioka <rikuto.nishioka@dena.com>
2024-04-01 02:31:15 +0900

    add link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <main>
       <h3>西岡について</h3>
       <p>作成中です</p>
+      <Link href="https://cook.nishioka-app.com" target="_blank">
+        西岡の花婿修行
+      </Link>
     </main>
   );
 }
